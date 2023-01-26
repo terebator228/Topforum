315a91b1badc4edbd420433b1430aa5488c1ff00
terebator228 <mish.8@mail.ru>
2023-01-26 16:20:58 +0300

    Верстка слайдера

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const slider = tns({
+    container: '.forums__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    responsive: {
+        320: {
+            nav: true,
+            mouseDrag: true
+        },
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1
+        },
+        700: {
+          gutter: 30
+        },
+        900: {
+          items: 1,
+          nav: false,
+        }
+      }
+});
+
+document.querySelector('.prev').addEventListener('click',function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click',function () {
+    slider.goTo('next');
+});
