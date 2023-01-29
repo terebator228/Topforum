c220f3ef2fb5073fd838707802b6122a6e0a565f
terebator228 <mish.8@mail.ru>
2023-01-29 14:26:21 +0300

    Верстка карусели отзывов

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,36 @@ document.querySelector('.prev').addEventListener('click',function () {
 document.querySelector('.next').addEventListener('click',function () {
     slider.goTo('next');
 });
+
+const reviewsSlider = tns({
+    container: '.reviews__inner',
+    items: 2,
+    slideBy: 'page',
+    autoplay: false,
+    responsive: {
+        320: {
+            nav: true,
+            mouseDrag: true,
+        },
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1
+        },
+        700: {
+          gutter: 30
+        },
+        900: {
+          items: 2,
+          nav: false,
+        }
+      }
+});
+
+document.querySelector('.prev_review').addEventListener('click',function () {
+    reviewsSlider.goTo('prev');
+});
+
+document.querySelector('.next_review').addEventListener('click',function () {
+    reviewsSlider.goTo('next');
+});
